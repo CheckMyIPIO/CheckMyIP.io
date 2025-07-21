595c74a803de2b44f58464b2813285b7a88ccb38
CheckMyIPIO <enricomartins325@gmail.com>
2025-07-21 16:06:28 -0400

    script.js

--- script.js
+++ script.js
@@ -54,3 +54,8 @@ L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> & contributors | <a href="https://carto.com/">Carto</a>',
 }).addTo(map);
+// Handle signup form
+document.getElementById("signup-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("signup-msg").classList.remove("hidden");
+});
